0b8db66e50701006270ba48e31ad73cb83c81e33
Alex Turchinovich <byalexes@gmail.com>
2021-02-24 20:07:25 +0300

    24.02.21

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const hb=document.querySelector('.header_humburger');
+const menu=document.querySelector('.fount');
+
+hb.addEventListener('click',function (){
+    menu.classList.toggle('active');
+})
