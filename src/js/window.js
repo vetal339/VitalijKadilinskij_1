7e19fc28a1c461f0f5bc2d79f8da54145779db88
Alex Turchinovich <byalexes@gmail.com>
2021-03-11 16:56:45 +0300

    11.03.21

---
+++ src/js/window.js
@@ -0,0 +1,5 @@
+const win=document.querySelector('.more_6');
+const kjkj=document.querySelector('.windowvisible');
+win.addEventListener('click',function (){
+    kjkj.classList.toggle('active');
+})
