ef697e1e5c8d5f5b74db67191fe15f7806f094fa
Alex Turchinovich <byalexes@gmail.com>
2021-03-23 17:43:07 +0300

    23.03.21

---
+++ src/js/window.js
@@ -0,0 +1,72 @@
+document
+    .querySelector('.more_1')
+    .addEventListener('click',function (){
+    let modalID=this.getAttribute('data-target');
+    document.querySelector(`#${modalID}`).style.display = "block";
+});
+document
+    .querySelector('.more_2')
+    .addEventListener('click',function (){
+        let modalID=this.getAttribute('data-target');
+        document.querySelector(`#${modalID}`).style.display = "block";
+    });
+document
+    .querySelector('.more_3')
+    .addEventListener('click',function (){
+        let modalID=this.getAttribute('data-target');
+        document.querySelector(`#${modalID}`).style.display = "block";
+    });
+document
+    .querySelector('.more_4')
+    .addEventListener('click',function (){
+        let modalID=this.getAttribute('data-target');
+        document.querySelector(`#${modalID}`).style.display = "block";
+    });
+document
+    .querySelector('.more_5')
+    .addEventListener('click',function (){
+        let modalID=this.getAttribute('data-target');
+        document.querySelector(`#${modalID}`).style.display = "block";
+    });
+document
+    .querySelector('.more_6')
+    .addEventListener('click',function (){
+        let modalID=this.getAttribute('data-target');
+        document.querySelector(`#${modalID}`).style.display = "block";
+    });
+document
+    .querySelector('.closewindow_1')
+    .addEventListener('click',function (){
+        let modalID=this.getAttribute('data-target');
+        document.querySelector(`#${modalID}`).style.display = "none";
+    });
+document
+    .querySelector('.closewindow_2')
+    .addEventListener('click',function (){
+        let modalID=this.getAttribute('data-target');
+        document.querySelector(`#${modalID}`).style.display = "none";
+    });
+document
+    .querySelector('.closewindow_3')
+    .addEventListener('click',function (){
+        let modalID=this.getAttribute('data-target');
+        document.querySelector(`#${modalID}`).style.display = "none";
+    });
+document
+    .querySelector('.closewindow_4')
+    .addEventListener('click',function (){
+        let modalID=this.getAttribute('data-target');
+        document.querySelector(`#${modalID}`).style.display = "none";
+    });
+document
+    .querySelector('.closewindow_5')
+    .addEventListener('click',function (){
+        let modalID=this.getAttribute('data-target');
+        document.querySelector(`#${modalID}`).style.display = "none";
+    });
+document
+    .querySelector('.closewindow_6')
+    .addEventListener('click',function (){
+        let modalID=this.getAttribute('data-target');
+        document.querySelector(`#${modalID}`).style.display = "none";
+    });
